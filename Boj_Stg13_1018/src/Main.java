0ea698a9e45bfa63a0060eb0f4361ba1f42d8282
JiwooKim-dev <jiwoo.kim.dev@gmail.com>
2020-04-24 02:16:02 +0900

    Update 1018 '체스판 다시 칠하기' (Error Fixed)

--- Boj_Stg13_1018/src/Main.java
+++ Boj_Stg13_1018/src/Main.java
@@ -16,26 +16,50 @@ public class Main {
 		int N = Integer.parseInt(tk.nextToken());
 		int M = Integer.parseInt(tk.nextToken());
 		
-		String lines[] = new String[N];
-		for (int i=0 ; i<lines.length ; i++){
-			lines[i] = br.readLine();
+		char board[][] = new char[N][M];
+		for (int i=0 ; i<N ; i++){
+			tk = new StringTokenizer(br.readLine());
+			String line = tk.nextToken();
+			for (int j=0 ; j<M ; j++){
+				board[i][j] = line.charAt(j);
+			}
 		}
 		
 		int min = 64;
 		
 		for (int startX=0 ; startX+8 <= M ; startX++){
 			for (int startY=0 ; startY+8 <= N ; startY++){
-				char base = lines[startX].charAt(startY);
 				int count = 0;
+				// 베이스 'W'
 				for (int i=startX ; i<startX+8 ; i++){
 					for (int j=startY ; j<startY+8 ; j++){
 						if ((i-startX + j-startY) % 2 == 0){
-							if (lines[j].charAt(i) != base){
+							if (board[j][i] == 'B'){
 								count++;
 							}
 						}
 						else {
-							if (lines[j].charAt(i) == base){
+							if (board[j][i] == 'W'){
+								count++;
+							}
+						}
+					}
+				}
+				if (count < min){
+					min = count;
+				}
+				
+				// 베이스 'B'
+				count = 0;
+				for (int i=startX ; i<startX+8 ; i++){
+					for (int j=startY ; j<startY+8 ; j++){
+						if ((i-startX + j-startY) % 2 == 0){
+							if (board[j][i] == 'W'){
+								count++;
+							}
+						}
+						else {
+							if (board[j][i] == 'B'){
 								count++;
 							}
 						}
