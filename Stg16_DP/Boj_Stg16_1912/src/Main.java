1328a992f17189bbd9c8c4fb7294663be4cad747
jiwookim <43198798+jiwoo-kimm@users.noreply.github.com>
2020-07-27 18:13:35 +0900

    Update Main.java

--- Stg16_DP/Boj_Stg16_1912/src/Main.java
+++ Stg16_DP/Boj_Stg16_1912/src/Main.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-// ¹éÁØ 1912 '¿¬¼ÓÇÕ'
+// Â¹Ã©ÃÃ˜ 1912 'ì—°ì†í•©'
 // DP
 // 2020.07.25
 
